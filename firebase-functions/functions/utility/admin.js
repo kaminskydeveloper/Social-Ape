f30b81ed4198cf165ad055b2c2ebc6044b483083
kaminskydeveloper <kaminskydev@gmail.com>
2019-11-23 00:16:01 +0100

    onScreamDelete and onUserImageChange triggers added

--- firebase-functions/functions/utility/admin.js
+++ firebase-functions/functions/utility/admin.js
@@ -2,8 +2,8 @@ var admin = require('firebase-admin');
 const config = require('../utility/config');
 
 admin.initializeApp({
-  //credential: admin.credential.applicationDefault(),
-  credential: admin.credential.cert(process.env.SERVICE_ACCOUNT),
+  credential: admin.credential.applicationDefault(),
+  //credential: admin.credential.cert(process.env.SERVICE_ACCOUNT),
   storageBucket: config.storageBucket,
   databaseURL: 'https://socialape-921a1.firebaseio.com',
 });
