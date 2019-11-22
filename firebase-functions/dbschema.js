2542d80f1b2c9c7000937bbd60127137579f162d
kaminskydeveloper <kaminskydev@gmail.com>
2019-11-22 19:36:09 +0100

    comment route added

--- firebase-functions/dbschema.js
+++ firebase-functions/dbschema.js
@@ -1,4 +1,16 @@
 let db = {
+  users: [
+    {
+      userId: 'asdasd',
+      email: 'user@email.com',
+      handle: 'user',
+      createdAt: '2019-11-21T18:47:50.061Z',
+      imageUrl: 'image/asdasdasdasd',
+      bio: 'Hello my name is...',
+      website: 'https://website.com',
+      location: 'London, UK',
+    },
+  ],
   screams: [
     {
       userHandle: 'user',
@@ -6,6 +18,14 @@ let db = {
       createdAt: '2019-11-21T18:47:50.061Z',
       likeCount: 5,
       commentCount: 2,
+    },
+  ],
+  comments: [
+    {
+      userHandle: 'user',
+      screamId: 'asdasfgafs',
+      body: 'nice one mate',
+      createdAt: '2019-11-21T18:47:50.061Z',
     },
   ],
 };
--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -7,17 +7,35 @@ require('dotenv').config();
 
 const FBAuth = require('./utility/fbAuth');
 
-const { getAllScreams, postOneScream } = require('./handlers/screams');
+const {
+  getAllScreams,
+  postOneScream,
+  getScream,
+  commentOnScream,
+} = require('./handlers/screams');
 
-const { signup, login, uploadImage } = require('./handlers/users');
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser,
+} = require('./handlers/users');
 
 //Scream routes
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
+app.get('/scream/:screamId', getScream);
+//TODO: delete scream
+//TODO: like a scream
+//TODO: unlike a scream
+app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
 
 // Users routes
 app.post('/signup', signup);
 app.post('/login', login);
 app.post('/user/image', FBAuth, uploadImage);
+app.post('/user', FBAuth, addUserDetails);
+app.get('/user', FBAuth, getAuthenticatedUser);
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
