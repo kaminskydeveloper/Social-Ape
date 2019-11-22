722bfef07fc0c5501830873f1092b33fa6de113b
kaminskydeveloper <kaminskydev@gmail.com>
2019-11-22 20:47:25 +0100

    like unlike delete routes added

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -12,6 +12,9 @@ const {
   postOneScream,
   getScream,
   commentOnScream,
+  likeScream,
+  unlikeScream,
+  deleteScream,
 } = require('./handlers/screams');
 
 const {
@@ -26,9 +29,9 @@ const {
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
 app.get('/scream/:screamId', getScream);
-//TODO: delete scream
-//TODO: like a scream
-//TODO: unlike a scream
+app.delete('/scream/:screamId', FBAuth, deleteScream);
+app.get('/scream/:screamId/like', FBAuth, likeScream);
+app.get('/scream/:screamId/unlike', FBAuth, unlikeScream);
 app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
 
 // Users routes
