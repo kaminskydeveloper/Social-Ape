af229f908b314895d8bd0dfe605e97fb7f17ebbd
kaminskydeveloper <kaminskydev@gmail.com>
2019-12-01 20:06:25 +0100

    upload image added

--- socialape-client/src/redux/actions/userActions.js
+++ socialape-client/src/redux/actions/userActions.js
@@ -65,3 +65,15 @@ const setAuthorizationHeader = token => {
   localStorage.setItem('FBIdToken', `Bearer ${token}`);
   axios.defaults.headers.common['Authorization'] = FBIdToken;
 };
+
+export const uploadImage = formData => dispatch => {
+  dispatch({ type: LOADING_USER });
+  axios
+    .post('/user/image', formData)
+    .then(() => {
+      dispatch(getUserData());
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
